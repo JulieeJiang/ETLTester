61e9aeb7d3341386b5c0a9b9315e0dbe850c85d0
Zhang Kang <kang.zhang@hp.com>
2013-06-04 17:30:20 +0800

    cli
    完成了new

--- lib/Framework/Assistant.rb
+++ lib/Framework/Assistant.rb
@@ -1,6 +1,6 @@
 module ETLTester
 
-	module Util
+	module Framework
 
 		module Assistant
 
@@ -15,9 +15,12 @@ module ETLTester
 				Dir.mkdir("#{project_dir}/mappings")
 				# Folder used for store config.yaml
 				Dir.mkdir("#{project_dir}/configuration")
-				# Folder used for store config.yaml
+				# Folder used for store tes suite
 				Dir.mkdir("#{project_dir}/test suites")
-
+				# Folder used for store log
+				Dir.mkdir("#{project_dir}/logs")
+				# Folder used for store reports
+				Dir.mkdir("#{project_dir}/reports")
 
 				# Set default value.
 				File.open("#{project_dir}/configuration/config.yaml", 'w') do |file|
@@ -38,10 +41,49 @@ module ETLTester
   :password: }
 				end
 
-				
-				Dir.mkdir("#{project_dir}/logs")
+			end # sef.new_project
 
-				Dir.mkdir("#{project_dir}/reports")
+
+			def self.new_mapping mapping_name, path
+				Dir.mkpath(path)
+				if File.exist?(path + "/" + mapping_name)
+					raise StandError.new "#{path + "/" + mapping_name} is already existed."
+				else
+					mapping_name = mapping_name.gsub(/\.rb$/, '') if mapping_name =~ /\.rb$/
+					File.open(path + "/" + mapping_name + '.rb', 'w') do |f|
+						f.puts %Q{mapping("#{mapping_name}") do
+
+# declare table will be used
+#
+# e.g. :
+# declare_target_table 'target_table', :t
+# declare_source_table 'source_table1', :src1
+# declare_source_table "select * from source_table2 where lgcl_del_fg = 'n'", :src2
+# declare_cte_as "select * from source_table3", :src3
+
+
+
+
+# define mappings
+#
+# e.g. :
+#
+# mp t.pk, src1.pk
+#
+# m t.some_ky, do
+# 	left_join src2, 'src1.fk = src2.pk'
+# 	if src2.some_ky.nil?
+# 		0 # Not Found
+# 	else
+# 		src2.some_ky
+# 	end
+# end
+
+
+# Refer to "https://github.com/piecehealth/ETLTester" for more information.
+end}
+					end
+				end
 			end
 
 		end
---
+++ unittest/itr2/mappings/release22/server path mgmt/l3_srvr_d.rb
@@ -0,0 +1,31 @@
+mapping("l3_srvr_d") do
+
+# declare table will be used
+#
+# e.g. :
+# declare_target_table 'target_table', :t
+# declare_source_table 'source_table1', :src1
+# declare_source_table "select * from source_table2 where lgcl_del_fg = 'n'", :src2
+# declare_cte_as "select * from source_table3", :src3
+
+
+
+
+# define mappings
+#
+# e.g. :
+#
+# mp t.pk, src1.pk
+#
+# m t.some_ky, do
+# 	left_join src2, 'src1.fk = src2.pk'
+# 	if src2.some_ky.nil?
+# 		0 # Not Found
+# 	else
+# 		src2.some_ky
+# 	end
+# end
+
+
+# Refer to "https://github.com/piecehealth/ETLTester" for more information.
+end
